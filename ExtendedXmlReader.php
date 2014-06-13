5e8a0f311811b0489c8a93be03086a1296f06178
Mieszko Malawksi <miechuliv@tlen.pl>
2014-06-13 11:44:24 +0200

    First commit

---
+++ ExtendedXmlReader.php
@@ -0,0 +1,60 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: USER
+ * Date: 13.06.14
+ * Time: 11:16
+ * To change this template use File | Settings | File Templates.
+ */
+
+class ExtendedXmlReader extends XMLReader{
+
+
+    /**
+     * finds certain xml node by path, eg: catalog > products > product
+     * @param $path
+     */
+    public function findNodeByPath($path)
+    {
+        $lastElement = $path[count($path)-1];
+        foreach($path as $pathElement)
+        {
+            $foundElement = false;
+            while(!$foundElement && $this->next())
+            {
+                if ($this->nodeType == XMLReader::ELEMENT) {
+                    $exp = $this->expand();
+
+                    if ($exp->nodeName == $pathElement)
+                    {
+                        $foundElement = true;
+
+                    }
+
+                }
+            }
+
+
+            if($foundElement)
+            {
+                // if not end of path
+                if($pathElement != $lastElement)
+                {
+                    // go one level deeper
+
+                    $this->read();
+                    // go to next path element
+                    continue;
+                }
+                else
+                {
+                    return $exp->nodeName;
+                }
+
+            }
+        }
+
+        return false;
+    }
+
+}
---
+++ tests/CoreTest.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: USER
+ * Date: 13.06.14
+ * Time: 11:24
+ * To change this template use File | Settings | File Templates.
+ */
+
+include_once(__DIR__.'/../ExtendedXmlReader.php');
+class CoreTest extends PHPUnit_Framework_TestCase{
+
+        public function testFindNode()
+        {
+            $reader = new ExtendedXmlReader();
+            $reader->open(__DIR__.'/files/test.xml');
+
+            $nodeName = $reader->findNodeByPath(array(
+                'catalog',
+                'products',
+                'product'
+            ));
+
+            $this->assertEquals('product',$nodeName);
+        }
+}
